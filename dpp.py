ece6f8df3018c87589ab66b0d3aad3f4add83ffa
Xaratustrah <shahab.sanjari@gmail.com>
2017-01-19 12:38:08 +0100

    changed using read_samples

--- dpp.py
+++ dpp.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """
 Determine and plot delta p over p from Schottky data
 
@@ -13,6 +15,9 @@ import sys, os
 GAMMA = 1.1384857688249141  # 238-U-89+ @ 129 MeV/u
 GAMMA_T = 2.44  # changed between 2.3 and 2.44
 
+LFRAMES = 2 ** 12
+NFRAMES = 100
+
 
 def get_eta(gamma, gamma_t):
     eta = (1 / gamma ** 2) - (1 / gamma_t ** 2)
@@ -21,12 +26,13 @@ def get_eta(gamma, gamma_t):
 
 def get_dpp(filename):
     iq_data = TIQData(filename)
-    iq_data.read(100, 2 ** 12, 1)
+    # iq_data.read(NFRAMES, LFRAMES, 1)
+    iq_data.read_samples(NFRAMES * LFRAMES, offset=0)
     iq_data.window = 'hamming'
 
     filename_woe = os.path.splitext(filename)[0]
 
-    xx, yy, zz = iq_data.get_spectrogram()
+    xx, yy, zz = iq_data.get_spectrogram(NFRAMES, LFRAMES)
     plot_spectrogram_dbm(xx, yy, zz)
     plt.savefig('{}_spec.png'.format(filename_woe))
 
