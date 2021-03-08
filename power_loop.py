7a3dc35098e28e6a9419e6b86323a4a04d6ea851
xaratustrah <shahab.sanjari@gmail.com>
2021-03-08 14:35:44 +0100

    update

--- power_loop.py
+++ power_loop.py
@@ -12,14 +12,16 @@ if several files are provided, then they are aligned correctly in time axis acco
 import os
 import sys
 import datetime
+from pprint import pprint
 from iqtools import *
 
 
 def find_power(iq_object, file_offset):
     # read one second worth of data at the offset go through the file
-    # TODO: here is the problem
-    iq_object.read_samples(
-        int(iq_object.fs), offset=file_offset)
+    # corresponds to how many frames each 1024 points?
+    nframes = int(iq_object.fs / 1024)
+    iq_object.read(nframes=1,
+                   lframes=int(iq_object.fs), sframes=file_offset)
 
     window = 50000  # Hz
     # lf = 2048
@@ -39,7 +41,8 @@ def find_power(iq_object, file_offset):
 
 
 def main():
-    results = []
+    # results = []
+    res_arr = np.array([])
 
     # Processing the first file
     first_ilename = sys.argv[1]
@@ -52,49 +55,50 @@ def main():
 
     dt_first_file = datetime.datetime.strptime(
         iq.date_time[:-9], '%Y-%m-%dT%H:%M:%S.%f')
-
+    ts_first_file = round(dt_first_file.timestamp())
+    print('Time stamp of the first file:', ts_first_file)
     # make the total length round to one second
     length_total = int(iq.nsamples_total / iq.fs)
 
     # going inside the first file
     for file_offset in range(length_total):
         iq_pwr = find_power(iq, file_offset)
-        results.append([iq_pwr, file_offset, file_offset])
+        # results.append([iq_pwr, file_offset, file_offset])
+        res_arr = np.append(res_arr, [file_offset, round(
+            dt_first_file.timestamp()) + file_offset, iq_pwr])
 
-    if (sys.argv) == 2:
-        exit()
-    # now if more files are there loop through all of them
+    if len(sys.argv) > 2:
+        # now if more files are there loop through all of them
 
-    for filename in sys.argv[2:]:
-        print('Processing file: ' + filename)
-        iq = get_iq_object(filename)
-        iq.read_samples(1)
+        for filename in sys.argv[2:]:
+            print('Processing file: ' + filename)
+            iq = get_iq_object(filename)
+            iq.read_samples(1)
 
-        # Datetime is of the kind:
-        # 2021 - 02 - 26T23: 22: 13.719023510 - 08: 00
+            # Datetime is of the kind:
+            # 2021 - 02 - 26T23: 22: 13.719023510 - 08: 00
 
-        dt_current_file = datetime.datetime.strptime(
-            iq.date_time[:-9], '%Y-%m-%dT%H:%M:%S.%f')
+            dt_current_file = datetime.datetime.strptime(
+                iq.date_time[:-9], '%Y-%m-%dT%H:%M:%S.%f')
 
-        # make the total length round to one second
-        length_total = int(iq.nsamples_total / iq.fs)
+            # make the total length round to one second
+            length_total = int(iq.nsamples_total / iq.fs)
 
-        # going through each file
-        for file_offset in range(length_total):
-            iq_pwr = find_power(iq, file_offset)
-            results.append([iq_pwr, int(
-                file_offset + (dt_current_file - dt_first_file).total_seconds()), file_offset])
+            # going through each file
+            for file_offset in range(length_total):
+                ts_current_file = round(dt_current_file.timestamp())
+                iq_pwr = find_power(iq, file_offset)
+                res_arr = np.append(res_arr, [
+                    ts_current_file - ts_first_file + file_offset, ts_first_file + file_offset, iq_pwr])
 
-    res_arr = np.array(results)
-    # from pprint import pprint
-    # pprint(res_arr)
-    # plot_spectrum(res_arr[:, 1], res_arr[:, 0], filename='plot')
+    res_arr = np.reshape(res_arr, (int(len(res_arr) / 3), 3))
     np.savetxt('{}_results.csv'.format(first_ilename),
-               res_arr[:, 0:2], delimiter=',')
+               res_arr, delimiter=',')
     write_spectrum_to_root(
-        res_arr[:, 1], res_arr[:, 0], '{}_results.root'.format(first_ilename))
+        res_arr[:, 0], res_arr[:, 2], '{}_results.root'.format(first_ilename))
     plt.figure()
-    plt.plot(res_arr[:, 1], res_arr[:, 0], 'ro')
+    plt.plot(res_arr[:, 0], res_arr[:, 2], 'r.')
+    plt.grid()
     plt.savefig('{}_results.png'.format(first_ilename))
     plt.close()
 
